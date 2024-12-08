ef32cbadfd9d909332e4c5897f4dfd9790185fc6
bvcxza <175357591+bvcxza@users.noreply.github.com>
2024-12-08 12:16:15 -0300

    send event to mutliple relays

--- src/commands/event_cmd.cpp
+++ src/commands/event_cmd.cpp
@@ -45,56 +45,66 @@ bool event_cmd::execute(int argc, char* argv[])
 		ssl::context ctx{ssl::context::tlsv12_client};
 		ctx.set_default_verify_paths();
 		tcp::resolver resolver{ioc};
-		websocket::stream<ssl::stream<tcp::socket>> ws{ioc, ctx};
-		std::string host_address = argv[5];
-		auto&& [host, port] = split_pair(host_address, ':');
-		auto const results = resolver.resolve(host, port);
-		net::connect(beast::get_lowest_layer(ws), results);
-
-		if (!SSL_set_tlsext_host_name(ws.next_layer().native_handle(), host.c_str()))
-			throw beast::system_error(
-				beast::error_code(
-					static_cast<int>(::ERR_get_error()),
-					net::error::get_ssl_category()),
-				"Failed to set SNI Hostname");
-
-
-		ws.next_layer().handshake(ssl::stream_base::client);
-
-		ws.set_option(websocket::stream_base::decorator(
-		[](websocket::request_type& req)
-		{
-			req.set(http::field::user_agent,
-				std::string(BOOST_BEAST_VERSION_STRING) + " sonos");
-		}));
 
 		nostr nstr { argv[2] };
-
 		uint16_t kind = std::atoi(argv[3]);
 		std::string tags = argv[4];
-		//user metadata : {name: <username>, about: <string>, picture: <url, string>}
 		std::vector<std::string> lines;
 		for (std::string line; std::getline(std::cin, line); lines.push_back(line));
 		std::string content = boost::algorithm::join(lines, "\\n");
-
 		std::string event = nstr.make_event(kind, content, tags);
-		std::cout << "handshaking to " << host_address << std::endl;
-		ws.handshake(host_address, "/");
-		ws.write(net::buffer(event));
-		std::cout << "write: " << event << std::endl;
-		beast::flat_buffer buffer;
-		ws.read(buffer);
-		ws.close(websocket::close_code::normal);
-		std::cout << "read: " << beast::make_printable(buffer.data()) << std::endl;
-		// ["OK","23f8ced2dd2fe3e8f61e11b60d870bcbdbe0d922b509df7b940c2e8925855b97",true,""]
-		// ["NOTICE","ERROR: bad msg: unparseable message"]
-		auto msg = beast::buffers_to_string(buffer.data());
-		// TODO
-		return msg.find("OK") != msg.npos;
+		bool result = false;
+		for (int i = 5; i < argc; ++i)
+		{
+			std::string host_address = argv[i];
+			auto&& [host, port] = split_pair(host_address, ':');
+			try
+			{
+				websocket::stream<ssl::stream<tcp::socket>> ws{ioc, ctx};
+				auto const results = resolver.resolve(host, port);
+				net::connect(beast::get_lowest_layer(ws), results);
+
+				if (!SSL_set_tlsext_host_name(ws.next_layer().native_handle(), host.c_str()))
+					throw beast::system_error(
+						beast::error_code(
+							static_cast<int>(::ERR_get_error()),
+							net::error::get_ssl_category()),
+						"Failed to set SNI Hostname");
+
+
+				ws.next_layer().handshake(ssl::stream_base::client);
+
+				ws.set_option(websocket::stream_base::decorator(
+				[](websocket::request_type& req)
+				{
+					req.set(http::field::user_agent,
+						std::string(BOOST_BEAST_VERSION_STRING) + " sonos");
+				}));
+
+				std::cout << "handshaking to " << host_address << std::endl;
+				ws.handshake(host_address, "/");
+				ws.write(net::buffer(event));
+				std::cout << "write: " << event << std::endl;
+				beast::flat_buffer buffer;
+				ws.read(buffer);
+				ws.close(websocket::close_code::normal);
+				std::cout << "read [" << host_address << "]: " << beast::make_printable(buffer.data()) << std::endl;
+				// ["OK","23f8ced2dd2fe3e8f61e11b60d870bcbdbe0d922b509df7b940c2e8925855b97",true,""]
+				// ["NOTICE","ERROR: bad msg: unparseable message"]
+				auto msg = beast::buffers_to_string(buffer.data());
+				// TODO
+				result = msg.find("OK") != msg.npos;
+			}
+			catch(const std::exception& e)
+			{
+				std::cerr << "Error [" << host_address << "]: " << e.what() << std::endl;
+			}
+		}
+		return result;
 	}
 	catch(const std::exception& e)
 	{
-		std::cerr << argv[0] << argv[1] << argv[2] << " Error: " << e.what() << std::endl;
+		std::cerr << argv[0] << ' ' << argv[1] << ' ' << argv[2] << " Error: " << e.what() << std::endl;
 		return false;;
 	}
 
