929917e682339293a4641dfddaacc3c2b3d18cec
bvcxza <175357591+bvcxza@users.noreply.github.com>
2024-12-16 17:02:49 -0300

    improve relays use

--- samples/bot.sh
+++ samples/bot.sh
@@ -4,7 +4,8 @@
 # bot private key (nsec)
 bot_nsec="nsec1...changethis"
 # bot write relays
-bot_w_relays="nostr.bitcoiner.social:443 nos.lol:443"
+bot_w_relays="nostr.bitcoiner.social:443 nos.lol:443 purplerelay.com:443"
+default_r_relays="$bot_w_relays relay.nostr.band:443 relay.damus.io:443"
 
 bot_npub=$(sonos keypair $bot_nsec | head -n1)
 bot_pubkey=$(sonos convert $bot_npub npub)
@@ -17,8 +18,8 @@ pubkey=\$(jq -r '.pubkey' \$1)
 npub=\$(sonos convert \$pubkey npub)
 npub_recipient=\$(jq -r '.content' \$1 | grep npub1 | sed -n -e 's/\(^\|\(.*\s\)\)tip\snostr:\(.\+\)\(\(\s.*\)\|$\)/\3/pi')
 printf '%s\n' $bot_w_relays > $relays_stage
-sonos relays read \$npub $bot_w_relays >> $relays_stage
-sonos relays read \$npub_recipient $bot_w_relays >> $relays_stage
+sonos relays read \$npub $default_r_relays >> $relays_stage
+sonos relays read \$npub_recipient $default_r_relays >> $relays_stage
 relays=\$(sort $relays_stage | uniq | xargs echo)
 pubkey_recipient=\$(sonos convert \$npub_recipient)
 tags="[\"e\",\"\$event_id\",\"\",\"reply\"],[\"p\",\"\$pubkey\",\"\",\"mention\"],[\"p\",\"\$pubkey_recipient\",\"\",\"mention\"]"
@@ -29,4 +30,4 @@ but maybe soon.
 EOF
 EOF0
 
-sonos req "{\"kinds\":[1],\"#p\":[\"$bot_pubkey\"],\"limit\":0}" "bash $script" $bot_w_relays
+sonos req "{\"kinds\":[1],\"#p\":[\"$bot_pubkey\"],\"limit\":0}" "bash $script" $default_r_relays
