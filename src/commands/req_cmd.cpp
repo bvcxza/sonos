938f00d016fadeed9309013d4d57fd0c0c0ba7c0
bvcxza <175357591+bvcxza@users.noreply.github.com>
2024-12-16 13:38:30 -0300

    multi relays req

--- src/commands/req_cmd.cpp
+++ src/commands/req_cmd.cpp
@@ -5,7 +5,12 @@
 #include <fstream>
 #include <iostream>
 #include <map>
+#include <memory>
+#include <mutex>
 #include <string>
+#include <thread>
+#include <unordered_set>
+#include <vector>
 
 #include <boost/beast/core.hpp>
 #include <boost/beast/websocket.hpp>
@@ -31,8 +36,7 @@ std::string req_cmd::help() const
 {
 	return R"(
 		Send a REQ message to nostr relays and execute a external command for each received event.
-		The program called by command receives the event JSON file path as the first parameter ($1 in bash)
-		and origin relay address ($2 in bash).
+		The program called by command receives the event JSON file path as the first parameter ($1 in bash).
 		Usage: sonos req <filters> <command> <relay_addresses ...>
 	)";
 }
@@ -45,60 +49,83 @@ bool req_cmd::execute(int argc, char* argv[])
 	{
 		using namespace boost::json;
 		net::io_context ioc;
+		tcp::resolver resolver{ioc};
 		ssl::context ctx{ssl::context::tlsv12_client};
 		ctx.set_default_verify_paths();
-		tcp::resolver resolver{ioc};
-		websocket::stream<ssl::stream<tcp::socket>> ws{ioc, ctx};
-		std::string host_address = argv[4];
-		auto&& [host, port] = split_pair(host_address, ':');
-		auto const results = resolver.resolve(host, port);
-		net::connect(beast::get_lowest_layer(ws), results);
-
-		if (!SSL_set_tlsext_host_name(ws.next_layer().native_handle(), host.c_str()))
-			throw beast::system_error(
-				beast::error_code(
-					static_cast<int>(::ERR_get_error()),
-					net::error::get_ssl_category()),
-				"Failed to set SNI Hostname");
-
-
-		ws.next_layer().handshake(ssl::stream_base::client);
-
-		ws.set_option(websocket::stream_base::decorator(
-		[](websocket::request_type& req)
-		{
-			req.set(http::field::user_agent,
-				std::string(BOOST_BEAST_VERSION_STRING) + " sonos");
-		}));
-
-		// ["REQ", <subscription_id>, <filters1>, <filters2>, ...]
 		std::string subscription_id = sha256(random(8));
-		std::string filters = argv[2];
-		std::string command = argv[3];
-		std::string req_msg = R"(["REQ","${subscription_id}",${filters}])";
-		assert(replaceAll(req_msg, {{"${subscription_id}",subscription_id},{"${filters}",filters}}));
-		std::cout << "req_msg: " << req_msg << std::endl;
-		std::cout << "handshaking to " << host_address << std::endl;
-		ws.handshake(host_address, "/");
-		ws.write(net::buffer(req_msg));
 		auto in_file_name = "sonos" + subscription_id;
-		std::filesystem::path in_file_path = std::filesystem::temp_directory_path() / in_file_name;
-		std::string fullCmd = "${command} ${in_file_path} ${host_address}";
-		assert(replaceAll(fullCmd, {{"${command}",command},{"${in_file_path}",in_file_path.string()},{"${host_address}",host_address}}));
-		for (;ws.is_open();)
+		const std::filesystem::path in_file_path = std::filesystem::temp_directory_path() / in_file_name;
+		// ["REQ", <subscription_id>, <filters1>, <filters2>, ...]
+		std::string req_msg = R"(["REQ","${subscription_id}",${filters}])";
+		std::string fullCmd = "${command} ${in_file_path}";
 		{
-			beast::flat_buffer buffer;
-			ws.read(buffer);
-			value vjson = parse(beast::buffers_to_string(buffer.data()));
-			auto&& evt_array = vjson.as_array();
-			if (evt_array.size() == 3 && value_to<std::string>(evt_array[0]) == "EVENT")
+			std::string filters = argv[2];
+			std::string command = argv[3];
+			assert(replaceAll(req_msg, {{"${subscription_id}",subscription_id},{"${filters}",filters}}));
+			assert(replaceAll(fullCmd, {{"${command}",command},{"${in_file_path}",in_file_path.string()}}));
+		}
+		std::mutex mutex;
+		std::unordered_set<std::string> evt_id_set; // TODO limit
+		auto handle_event = [&in_file_path,&fullCmd,&mutex,&evt_id_set](websocket::stream<ssl::stream<tcp::socket>>&& ws){
+			for (;ws.is_open();)
 			{
-				std::ofstream out(in_file_path);
-				out << serialize(evt_array[2].as_object());
-				out.close();
-				std::cout << "command: " << fullCmd << std::endl;
-				std::system(fullCmd.c_str());
+				beast::flat_buffer buffer;
+				ws.read(buffer);
+				value vjson = parse(beast::buffers_to_string(buffer.data()));
+				auto&& evt_array = vjson.as_array();
+				if (evt_array.size() == 3 && value_to<std::string>(evt_array[0]) == "EVENT")
+				{
+					std::scoped_lock lock(mutex);
+					auto&& evt_obj = evt_array[2].as_object();
+					auto&& evt_id = value_to<std::string>(evt_obj["id"]);
+					if (evt_id_set.contains(evt_id)) continue;
+
+					std::ofstream out(in_file_path);
+					out << serialize(evt_obj);
+					out.close();
+					std::cout << "command: " << fullCmd << std::endl;
+					std::system(fullCmd.c_str());
+
+					evt_id_set.insert(evt_id);
+				}
 			}
+		};
+		std::vector<std::thread> threads;
+		threads.reserve(argc - 4 - 1);
+		for (int i = 4; i < argc; ++i)
+		{
+			websocket::stream<ssl::stream<tcp::socket>> ws{ioc, ctx};
+			std::string host_address = argv[i];
+			auto&& [host, port] = split_pair(host_address, ':');
+			auto const results = resolver.resolve(host, port);
+			net::connect(beast::get_lowest_layer(ws), results);
+
+			if (!SSL_set_tlsext_host_name(ws.next_layer().native_handle(), host.c_str()))
+				throw beast::system_error(
+					beast::error_code(
+						static_cast<int>(::ERR_get_error()),
+						net::error::get_ssl_category()),
+					"Failed to set SNI Hostname");
+
+
+			ws.next_layer().handshake(ssl::stream_base::client);
+
+			ws.set_option(websocket::stream_base::decorator(
+			[](websocket::request_type& req)
+			{
+				req.set(http::field::user_agent,
+					std::string(BOOST_BEAST_VERSION_STRING) + " sonos");
+			}));
+
+			std::cout << "req_msg: " << req_msg << std::endl;
+			std::cout << "handshaking to " << host_address << std::endl;
+			ws.handshake(host_address, "/");
+			ws.write(net::buffer(req_msg));
+
+			if (i < (argc - 1))
+				threads.emplace_back(handle_event, std::move(ws));
+			else // last job in current thread
+				handle_event(std::move(ws));
 		}
 	}
 	catch(const std::exception& e)
